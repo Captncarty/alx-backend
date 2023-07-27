5de4238e434b8611a683d213040239673a8db9bd
Kelvin <edithmukoro3@gmail.com>
2023-07-27 02:14:32 +0100

    advanced task fix

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -6,38 +6,44 @@ and is a caching system:
 
 
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class LFUCache(BaseCaching):
     """
-    LFUCache class inherint parent class BasicCaching
+    LFU caching system
     """
-
     def __init__(self):
+        """ Initialize class instance. """
         super().__init__()
-        self.cache_data = {}
-        self.lfu = []
+        self.cache_data = OrderedDict()
+        self.mru = ""
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """
+        Add an item in the cache
         """
         if key and item:
-            if key in self.cache_data:
-                self.cache_data[key] = item
-                self.lfu.remove(key)
-            else:
-                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    discarded = self.lfu.pop(0)
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key in self.cache_data:
+                    self.cache_data.update({key: item})
+                    self.mru = key
+                else:
+                    """
+                    discard the most recently used item
+                    """
+                    discarded = self.mru
                     del self.cache_data[discarded]
-                    print('DISCARD: {}'.format(discarded))
+                    print("DISCARD: {}".format(discarded))
+                    self.cache_data[key] = item
+                    self.mru = key
+            else:
                 self.cache_data[key] = item
-            self.lfu.append(key)
+                self.mru = key
 
     def get(self, key):
         """ Get an item by key
         """
         if key in self.cache_data:
-            self.lfu.remove(key)
-            self.lfu.append(key)
+            self.mru = key
             return self.cache_data[key]
-        return None
