6e1b66128f52e38d397405594ecb516989e3ab61
Kelvin <edithmukoro3@gmail.com>
2023-07-27 02:05:33 +0100

    advanced task

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Create a class LFUCache that inherits from BaseCaching
+and is a caching system:
+"""
+
+
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """
+    LFUCache class inherint parent class BasicCaching
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = {}
+        self.lfu = []
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.lfu.remove(key)
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    discarded = self.lfu.pop(0)
+                    del self.cache_data[discarded]
+                    print('DISCARD: {}'.format(discarded))
+                self.cache_data[key] = item
+            self.lfu.append(key)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            self.lfu.remove(key)
+            self.lfu.append(key)
+            return self.cache_data[key]
+        return None
